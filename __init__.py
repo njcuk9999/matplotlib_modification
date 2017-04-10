59d0eaa96d1f667a51e3c67b187ee162bc38fc7a
Neil James Cook <neil.james.cook@gmail.com>
2017-04-10 13:18:24 -0400

    modified init params

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from . import RelatedFormatter
 from . import RelatedLocator
 from . import rainbow_text
 from . import right_ascension_axis
+from . import core
 
 __author__ = "Neil Cook"
 __email__ = 'neil.james.cook@gmail.com'
@@ -9,8 +10,23 @@ __version__ = '0.1'
 __all__ = ['Formatter', 'Locator', 'TimeFormatter', 'RainbowText',
            'RightAscensionAxis']
 
+# =============================================================================
+# Tick functions
+# =============================================================================
 Formatter = RelatedFormatter.RelatedFormatter
 Locator = RelatedLocator.RelatedLocator
 TimeFormatter = RelatedFormatter.TimeFormatter
+# =============================================================================
+# Misc functions
+# =============================================================================
 RainbowText = rainbow_text.rtext
 RightAscensionAxis = right_ascension_axis.ra_axis
+# =============================================================================
+# Axis functions
+# =============================================================================
+EqualAxis = core.equal_axis
+
+# =============================================================================
+# Specific plot functions
+# =============================================================================
+XYHist = core.add_xy_hists
